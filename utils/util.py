5f1e30eb41888f1e69e271e01451bcb374f6448a
K-yng <kangjun205@gmail.com>
2025-02-04 01:52:41 +0900

    NK-63 fix: Fix 초성 mapping error

--- utils/util.py
+++ utils/util.py
@@ -23,6 +23,25 @@ def coll_name_mapping(text):
         'ㅞ': 'np', 'ㅟ': 'nl', 'ㅠ': 'b', 'ㅡ': 'm', 'ㅢ': 'ml',
         'ㅣ': 'l'
     }
+
+    cho_list = [
+        'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ',
+        'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+    ]
+    jung_list = [
+        'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ',
+        'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'
+    ]
+    jong_list = [
+        '', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ',
+        'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+    ]
+    
+    jong_complex = {
+        'ㄳ': ['ㄱ', 'ㅅ'], 'ㄵ': ['ㄴ', 'ㅈ'], 'ㄶ': ['ㄴ', 'ㅎ'], 'ㄺ': ['ㄹ', 'ㄱ'],
+        'ㄻ': ['ㄹ', 'ㅁ'], 'ㄼ': ['ㄹ', 'ㅂ'], 'ㄽ': ['ㄹ', 'ㅅ'], 'ㄾ': ['ㄹ', 'ㅌ'],
+        'ㄿ': ['ㄹ', 'ㅍ'], 'ㅀ': ['ㄹ', 'ㅎ'], 'ㅄ': ['ㅂ', 'ㅅ']
+    }
     
     result = ''
     for char in text:
@@ -30,12 +49,15 @@ def coll_name_mapping(text):
             char_code = ord(char) - ord('가')
             cho = char_code // 588 ## 초성
             jung = (char_code - (588 * cho)) // 28 ## 중성
-            jong = char_code % 28 ## 종성성
+            jong = char_code % 28 ## 종성
             
-            result += kor_to_eng[chr(ord('ㄱ') + cho)]
-            result += kor_to_eng[chr(ord('ㅏ') + jung)]
-            if jong > 0:
-                result += kor_to_eng[chr(ord('ㄱ') + jong - 1)]
+            result += kor_to_eng[cho_list[cho]]
+            result += kor_to_eng[jung_list[jung]]
+            if jong_list[jong] in jong_complex:
+                for divided_jong in jong_complex[jong_list[jong]]:
+                    result += kor_to_eng[divided_jong]
+            elif jong_list[jong]:
+                result += kor_to_eng[jong_list[jong]]
         else:
             result += char
     
