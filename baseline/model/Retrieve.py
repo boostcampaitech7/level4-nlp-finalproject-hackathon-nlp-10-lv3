188e9f1657d0150c3c3b498e01c4106aa495eef5
K-yng <kangjun205@gmail.com>
2025-02-08 18:53:12 +0900

    NK-62 docs: Add docstring to Retrieve.py

--- baseline/model/Retrieve.py
+++ baseline/model/Retrieve.py
@@ -6,6 +6,21 @@ from utils.coll_name_mapping import coll_name_mapping
 from loguru import logger
 
 class Retrieval():
+    """
+    주어진 카테고리 및 사용자 요구사항 기반으로 구체적인 장소 검색
+
+    각 장소별 요약된 리뷰를 카테고리 별로 저장한 vectorDB와 연결
+    주어진 카테고리에 맞춰 검색 수행
+    사용자 쿼리와 저장된 리뷰 기반으로
+    Dense와 Sparse method의 weighted sum을 활용한 Hybrid search 활용
+
+    Args:
+        query (str): 사용자 요구사항
+        w (float): Dense retrieval score에 적용할 weight값 (0에서 1사이의 값)
+        k (int): 검색할 장소의 수
+        place_ids (List[int]): 검색 장소 기준 일정 거리 내 장소들의 ID
+        api_key (str): Naver Clova Studio API key
+    """
     def __init__(
             self,
             query,
@@ -23,22 +38,53 @@ class Retrieval():
         return
     
     def load_DB(self):
+        """
+        VectorDB에 연결
+
+        Returns:
+            MilvusClient: VectorDB client
+        """
         URI = os.path.join(".", "db", "course_rcmd.db")
         return MilvusClient(URI)
     
     def close_DB(self):
+        """
+        VectorDB와 연결 해제
+        """
         self.client.close()
             
     def call_dense(self):
+        """
+        Dense Retrieval에 활용할 Clova Studio Embedding model 호출
+
+        Returns:
+            ClovaXEmbeddings: Query를 vector 형태의 embedding으로 변환
+        """
         return ClovaXEmbeddings(model="clir-emb-dolphin", api_key=self.api_key)
     
     def call_sparse(self):
+        """
+        Sparse Retrieval에 활용할 BM25 embedding model 호출
+
+        Returns:
+            BM25Embedding: Query를 vector 형태의 embedding으로 변환
+        """
         EMBEDDING_PATH = os.path.join(".", "model", "sparse_embedding.pkl")
         with open(EMBEDDING_PATH, 'rb') as f:
             sparse_embedding = pickle.load(f)
         return sparse_embedding
     
     def make_request(self, query, place_ids):
+        """
+        사용자 쿼리에 대해 vectorDB에 검색하기 위한 request 작성성
+
+        Args:
+            query (str): 사용자 요구사항
+            place_ids (List[str]): 검색 장소 기준 일정 거리 내 장소들의 ID
+
+        Returns:
+            List[AnnSearchRequest]: 검색할 내용에 대한 request 객체체
+        """
         dense_embedding = self.call_dense()
         sparse_embedding = self.call_sparse()
         dense_vector = dense_embedding.embed_query(query)
@@ -70,6 +116,15 @@ class Retrieval():
         return [dense_request, sparse_request]
     
     def search(self, category):
+        """
+        작성된 request 기반으로, hybrid search 수행행
+
+        Args:
+            category (str): 검색을 수행할 category
+
+        Returns:
+            List[Dict[str, str]]: 검색된 장소와 관련된 meta-data 및 review text
+        """
         res = self.client.hybrid_search(
             collection_name=coll_name_mapping(category),
             reqs=self.requests,
